f97ccd1161414c657d077c06caba1653f37bc3e6
raywlx <raywlx44@gmail.com>
2022-07-28 19:23:40 +0300

    add files

---
+++ bash.sh
@@ -0,0 +1,46 @@
+cd
+# change directory
+
+cd ..
+# change directory back
+
+touch file-name.txt
+# create file
+
+mv file1 location
+# move
+
+cp
+# copy
+
+rm
+# remove
+
+code
+# vs code
+
+pwd
+# current working directory
+
+~
+# home directory
+
+mkdir
+# make directory
+
+ls
+# list files
+
+cat 
+# read file 
+
+nano
+# text editor
+
+npm ci
+# node package manager clean install
+
+git add
+# add files to git (. to add everything)
+
+git commit
---
+++ functions.js
@@ -0,0 +1,14 @@
+ function testFunc(add) {
+     return 10 + add
+ }
+
+ function testFunc2() {
+     return 6
+ }
+
+
+ console.log(testFunc)
+ console.log(testFunc(3))
+ console.log(testFunc(36))
+ console.log(testFunc2)
+ console.log(testFunc2())
